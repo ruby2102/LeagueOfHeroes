f75397238b61cd1298fcc70ca554c9d1aa6de2bf
ruby2102 <diogo3002.spam@gmail.com>
2024-12-30 22:04:57 +0000

    v2

--- src/app/ManageHero/page.jsx
+++ src/app/ManageHero/page.jsx
@@ -3,18 +3,25 @@
 import styles from "../../styles/page.module.css";
 import { useSearchParams } from "next/navigation";
 import HeroForm from "../../Documentos/HeroForm";
+import { Suspense } from "react";
 
-const ManageHero = () => {
+const ManageHeroContent = () => {
     const searchParams = useSearchParams();
     const heroId = searchParams.get("id");
 
     return (
         <div className={styles.page}>
-
             <h1>{heroId ? `Editando Herói com ID: ${heroId}` : "Adicionando Novo Herói"}</h1>
-
             <HeroForm isEdit={!!heroId} />
         </div>
+    );
+};
+
+const ManageHero = () => {
+    return (
+        <Suspense fallback={<div>Carregando...</div>}>
+            <ManageHeroContent />
+        </Suspense>
     );
 };
 
